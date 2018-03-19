22150f74498705a92200742accb235258c166889
jmh081701 <jiangminghao@antiy.cn>
2018-03-19 22:14:21 +0800

    添加tcp-seq-data,ipid-data 获取测试逻辑

--- traffic_tool/traffic_tool/source.cpp
+++ traffic_tool/traffic_tool/source.cpp
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "BaseTool.h"
-#include "IPCBase.h"
-#include "EntroyEstimation.h"
-#include "Controller.h"
 #include <inaddr.h>
 #include <string.h>
 int main()
@@ -14,14 +11,13 @@ int main()
 	{
 		in_addr addr;
 		addr.S_un.S_addr =(unsigned int (p->first));
-		printf("%0.16s packets:%d \n",inet_ntoa(addr),(p->second).size());
+		printf("%0.16s (%X) packets:%d \n",inet_ntoa(addr),p->first,(p->second).size());
 	}
+	vector<_ipid_build> ipid_data = pkt.get_ipid_data(prst, inet_addr("172.16.30.34"));
+	vector< vector<_ipid_build> > ipid_sequences = pkt.construct_ipid_sequences(ipid_data);
 	char *filename = "data.file";
 	FILE *fp = fopen(filename, "w");
-	auto rst =pkt.get_host_count_data(NULL, inet_addr("192.168.199.122"));
-	vector< vector<_ipid_build> > ipid_sequences = pkt.construct_ipid_sequence(rst,60*15);
 
-	printf("size: %d \n", ipid_sequences.size());
 	fprintf(fp, "[");
 	for (int i = 0; i < ipid_sequences.size(); i++)
 	{
@@ -41,7 +37,13 @@ int main()
 		}
 	}
 	fprintf(fp, "]");
+
 	fclose(fp);
+	vector<_tcp_sequence_build> tcp_seq_data = pkt.get_tcp_seq_data(prst, inet_addr("172.16.30.34"));
+	vector< vector<_tcp_sequence_build>> tcp_seq_sequences = pkt.construct_tcp_sequences(tcp_seq_data);
+	vector< vector<int>> associate_ipid_seq = pkt.associate_ipidseq_tcpseqs(ipid_sequences, tcp_seq_sequences);
+	vector<_tcp_srcport_build> tcp_srcport_data = pkt.get_tcp_srcport_data(prst, inet_addr("172.16.30.34"));
+	auto srcport_sequences = pkt.construct_tcpsrcport_sequences(tcp_srcport_data);
 	//printf("new ip ");
 	//BaseTool pkt2("C:\\Users\\dell\\Documents\\detect_nat\\pcaps\\03_13_08_30_11_31_192_168_8_153_MacOS.pcap");
 
