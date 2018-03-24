f8746a960f8f3da30dfb4ac223f6371fbe9cebc9
jmh081701 <jiangminghao@antiy.cn>
2018-03-24 18:51:14 +0800

    流量统计特征示例代码.每天采集到流量后及时使用本文件的逻辑添加到特征库中。 其中需要注意ip,以及device_type字段

---
+++ traffic_tool/traffic_tool/generate_vector.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BaseTool.h"
+#include <inaddr.h>
+#include <string.h>
+#if 1
+int main()
+{
+	BaseTool pkt("C:\\Users\\dell\\Documents\\detect_nat\\pcaps\\nat_netflow\\03_13_21_50_09_10_172_16_30_34_windows_7.pcap");
+	char * ip = "172.16.30.34";
+	char device_type = 0x01;//02 nat;01 host
+	auto prst = pkt.cluster_raw_pakcets();
+	auto statics_feature = pkt.abstract_statics_feature(prst, inet_addr(ip), 60 * 12, device_type);
+	char buffer[512];
+	char * filename = ".\\data\\vectorize_data.dat";
+	FILE *fp = fopen(filename,"a");
+	for (int i = 0; i < statics_feature.size(); i++)
+	{
+		statics_feature[i].vectorize(fp);
+	}
+	in_addr addr;
+	fclose(fp);
+	//vector< vector<_tcp_srcport_build>> srcport_sequences;
+	//for (map < unsigned int, vector< _packet_chunk_> > ::iterator it = prst->begin(); it != prst->end(); it++)
+	//{
+	//	if (it->first != inet_addr(ip))
+	//	{
+	//		continue;
+	//	}
+	//	vector<_tcp_srcport_build> tcp_srcport_data = pkt.get_tcp_srcport_data(prst, it->first);
+	//	auto srcport_sequences_ = pkt.construct_tcpsrcport_sequences(tcp_srcport_data);
+
+	//	if (srcport_sequences_.size())
+	//	{
+	//		srcport_sequences = srcport_sequences_;
+	//		addr.S_un.S_addr = it->first;
+	//		printf("%s : srcport size:%d \n", inet_ntoa(addr), srcport_sequences.size());
+	//	}
+	//}
+	system("pause");
+}
+#endif
