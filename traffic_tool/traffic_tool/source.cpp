be54534063d88b31ff6307513663a787764f03a3
jmh081701 <jiangminghao@antiy.cn>
2018-03-24 18:42:59 +0800

    nat规模识别调用示例

--- traffic_tool/traffic_tool/source.cpp
+++ traffic_tool/traffic_tool/source.cpp
@@ -3,26 +3,49 @@
 #include "BaseTool.h"
 #include <inaddr.h>
 #include <string.h>
+#if 0
 int main()
 {
-	BaseTool pkt("C:\\Users\\dell\\Documents\\detect_nat\\pcaps\\03_21_14_30_15_30_172_16_30_34_windows_7.pcap");
-	char * ip = "172.16.30.34";
+	BaseTool pkt("C:\\Users\\dell\\Documents\\detect_nat\\pcaps\\nat_netflow\\03_23\\3407\\wan.pcap");
+	char * ip = "192.168.1.234";
 	auto prst = pkt.cluster_raw_pakcets();
-	vector<_tcp_srcport_build> tcp_srcport_data = pkt.get_tcp_srcport_data(prst, inet_addr(ip));
-	auto srcport_sequences = pkt.construct_tcpsrcport_sequences(tcp_srcport_data);
-	printf("srcport size:%d \n", srcport_sequences.size());
-	for (int i = 0; i < srcport_sequences.size(); i++)
+	auto statics_feature = pkt.abstract_statics_feature(prst, inet_addr(ip),60*12,0x02);
+	for (int i = 0; i < statics_feature.size(); i++)
 	{
-		printf("NUM(%d):\n", i);
-		for (int j = 0; j < srcport_sequences[i].size(); j++)
+		statics_feature[i].display();
+	}
+	in_addr addr;
+	vector< vector<_tcp_srcport_build>> srcport_sequences;
+	for (map < unsigned int, vector< _packet_chunk_> > ::iterator it = prst->begin(); it != prst->end(); it++)
+	{
+		if (it->first != inet_addr(ip))
 		{
-			printf("%d(%d)  ", srcport_sequences[i][j].srcport, srcport_sequences[i][j].timestamp);
-			if (j == (srcport_sequences[i].size() - 1))
-			{
-				printf("\n");
-			}
+			continue;
+		}
+		vector<_tcp_srcport_build> tcp_srcport_data = pkt.get_tcp_srcport_data(prst, it->first);
+		auto srcport_sequences_ = pkt.construct_tcpsrcport_sequences(tcp_srcport_data);
+		
+		if (srcport_sequences_.size())
+		{
+			srcport_sequences = srcport_sequences_;
+			addr.S_un.S_addr = it->first;
+			printf("%s : srcport size:%d \n", inet_ntoa(addr), srcport_sequences.size());
 		}
 	}
+	/*system("pause");
+	return 0;*/
+	//for (int i = 0; i < srcport_sequences.size(); i++)
+	//{
+	//	printf("NUM(%d):\n", i);
+	//	for (int j = 0; j < srcport_sequences[i].size(); j++)
+	//	{
+	//		printf("%d(%d)  ", srcport_sequences[i][j].srcport, srcport_sequences[i][j].timestamp);
+	//		if (j == (srcport_sequences[i].size() - 1))
+	//		{
+	//			printf("\n");
+	//		}
+	//	}
+	//}
 	char *filename = "data.file";
 	FILE *fp = fopen(filename, "w");
 
@@ -49,3 +72,4 @@ int main()
 	fclose(fp);
 	system("pause");
 }
+#endif
