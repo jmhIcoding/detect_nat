821bb879ca656ecab88dda2c50371b49e44ea898
jmh081701 <jiangminghao@antiy.cn>
2018-03-25 12:10:46 +0800

    添加在线获取主函数

--- traffic_tool/traffic_tool/generate_vector_online.cpp
+++ traffic_tool/traffic_tool/generate_vector_online.cpp
@@ -3,25 +3,76 @@
 #include "BaseTool.h"
 #include <inaddr.h>
 #include <string.h>
-#if 1
-int main()
+int str2int(char *str)
 {
-	char * ip = "172.16.30.34";
+	int rst = 0;
+	for (int i = 0; i<strlen(str); i++)
+	{
+		rst = rst * 10 + str[i] - '0';
+	}
+	return rst;
+}
+#if 1
+int main(int argc, char *argv[])
+{
+	if (argc != 6)
+	{
+		printf("usage:\
+			   			   				%s outfile(**.dat) ip_addr(172.16.30.29) device_type(nat or host) timegap(720) interface(eth2.2) \n", argv[0]);
+#ifdef WINDOWS
+		system("pause");
+#endif
+		exit(-1);
+	}
+	char * ip = argv[2];
 	char device_type = 0x01; //host
+	int timegap=str2int(argv[4]);
 	char filter[256] = { 0 };
-	//sprintf(filter, "host %s ", ip);
-	BaseTool pkt("\\Device\\NPF_{286C61F8-4979-4EF5-8087-CE3DFB534602}",filter);
-	char buf[256] = { 0 };
+	sprintf(filter, "host %s ", ip);
+	BaseTool pkt(argv[5], filter);
+	char buf[1024] = { 0 };
+	char *filename = argv[1];
+	FILE *fp = fopen(filename, "a");
+	if (fp == NULL)
+	{
+		printf("open %s error.\n", filename);
+#ifdef WINDOWS
+		system("pause");
+#endif
+		exit(-1);
+	}
+	if (strcmp(argv[3], "nat") == 0)
+	{
+		device_type = 0x02;
+	}
+	else if (strcmp(argv[3], "host") == 0)
+	{
+		device_type = 0x01;
+	}
+	else
+	{
+		printf("device_type reject.\n");
+#ifdef WINDOWS
+		system("pause");
+#endif
+		exit(-1);
+	}
+	int count = 0;
 	while (true)
 	{
-		auto prst = pkt.cluster_raw_pakcets_online(0, 1 * 60);
-		auto statics_feature = pkt.abstract_statics_feature(prst, inet_addr(ip), 60 * 1, device_type);
+		auto prst = pkt.cluster_raw_pakcets_online(0,timegap);
+		auto statics_feature = pkt.abstract_statics_feature(prst, inet_addr(ip), timegap, device_type);
 		for (int i = 0; i < statics_feature.size(); i++)
 		{
-			statics_feature[i]._vectorize(buf, 256);
-			sprintf("%s", buf);
+			statics_feature[i].vectorize(fp);
+			count++;
+		}
+		if (count % 20==0)
+		{
+			fflush(fp);
 		}
 
 	}
+	fclose(fp);
 }
 #endif
