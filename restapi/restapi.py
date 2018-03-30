694ff9817e3b2b0e3f990a8d57835c75fcab0886
jmh081701 <jiangminghao@antiy.cn>
2018-03-30 17:10:22 +0800

    添加restapi文件夹以及restapi.py，准备开始整合数据.

---
+++ restapi/restapi.py
@@ -0,0 +1,22 @@
+__author__ = 'dell'
+from  flask import  Flask
+from  flask import jsonify
+from flask import  request
+app=Flask(__name__)
+'''
+request format:
+{
+    "ip1":[,,,],
+    "ip2":[,,,],
+    "ip3":[,,,]
+}
+向量是一个19维的向量
+'''
+@app.route("/api/predict",methods=["POST"])
+def predicts():
+    req=request.json()
+    for each in req:
+        print(each)
+        print(req[each])
+    return "OK"
+app.run("0.0.0.0",port=9090)
