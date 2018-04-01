85e35e60f3e57c52493154036e9172fc6d1f09b3
DESKTOP-DJ29T4D\jmh081701 <1024836002@qq.com>
2018-04-02 07:53:07 +0800

    完成 PREDICT_WITH_DATAFLOW模块，修复若干bug,同时进行测试。

--- tool/PREDICT_WITH_DATAFLOW.py
+++ tool/PREDICT_WITH_DATAFLOW.py
@@ -3,10 +3,9 @@ __author__ = 'jmh081701'
 import  tensorflow as tf
 import  numpy as np
 import  json    # 使用ujson会快很多,import ujson as json
-
 class Predict(object):
     def __init__(self,modeldir):
-        dir =modeldir+"/"
+        dir =modeldir
         self.ckpt =tf.train.get_checkpoint_state(dir)
         self.saver=object()
         if self.ckpt and self.ckpt.model_checkpoint_path:
@@ -46,17 +45,19 @@ class Predict(object):
         rst=list()
         vec=list()
         lab=list()
+
         for eachLine in List:
-            line=eachLine.split(seq=' ')
+            #print(eachLine)
+            line=eachLine.split(sep=" ")
             sample={'ip':"192.168.1.1",'label':'0','vec':[]}
             sample['label']=line[1]
             sample['ip']=line[0]
-            for each in range(2,len(line)):
+            for i in range(2,len(line)):
                 if i in [16]:
                     continue
                     #过滤 diff udp-tcp 因为区分度
-                sample['vec'].append(float(each[i]))
-            sample['vec']=self.normalize_parameter(sample['vec'],self.normalize_parameter)
+                sample['vec'].append(float(line[i]))
+            sample['vec']=self.normalize_minmax(sample['vec'],self.normalize_parameter)
             vec.append(sample['vec'].copy())
             lab.append([0.,0.])
             sample['vec']=None
@@ -65,12 +66,18 @@ class Predict(object):
 
     def predict(self,inputList):
         try:
+            #print(inputList)
             info,vec,lab=self.vectorize(inputList[0:-1])
-            out=sess.run("outputY:0",feed_dict={"InputX:0":vec,"InputY:0":lab})
+            out=self.sess.run("outputY:0",feed_dict={"InputX:0":vec,"InputY:0":lab})
             outstring =""
             for i in range(0,len(info)):
-                outstring=info[i][ip]+" "+ out[i][1]+"\n"
+                outstring+=info[i]['ip']+" "+str( out[i][1])+";"
             return outstring
         except:
             raise Exception("Error input List.")
-
+if __name__ == '__main__':
+    cnnmodel = Predict(modeldir="./")
+    inputList="192.168.1.1 2 51691 77924749 50641 4770915 84444 100244 1651 640 2963 576 1515 653 2.000000 720.000000 0.002782 0.963462 0 2 14823.428863;192.168.30.34 2 115585 185072803 88677 5977789 178816 202565 1688 611 1925 605 1407 319 -1.000000 720.000000 -0.001391 0.983428 0 3 23074.247645;"
+    inputList=inputList.split(';')
+    out=cnnmodel.predict(inputList)
+    print(out)
