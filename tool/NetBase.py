531c3f5e375c9f1564469fea2665b3c3cbc28c42
DESKTOP-DJ29T4D\jmh081701 <1024836002@qq.com>
2018-04-04 12:06:41 +0800

    新增linux下网络模块:NetBase.py,通过启动这个模块，可以在线接受数据进行预测。

--- tool/NetBase.py
+++ tool/NetBase.py
@@ -2,27 +2,27 @@ __author__ = 'dell'
 import  define
 import  socket
 from PREDICT_WITH_DATAFLOW import  Predict
-address=("127.0.0.1",define.PPORT)
+address=(define.PHOST,define.PPORT)
 server_py=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server_py.bind(address)
 server_c=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-server_c.connect("127.0.0.1",define.CPORT)
+server_c.connect((define.CHOST,define.CPORT))
 
 if __name__ == '__main__':
     #预测时的主函数
     print("bind well")
-    cnnmodel=Predict(modeldir=".//")
+    cnnmodel=Predict(modeldir="./")
     while True:
         data,addr=server_py.recvfrom(4096)
         if not data:
             print("client has exist")
             break
-        print("data",data)
-        datas=data.decode(encoding='utf8').split('\n')[0:-1] #最后一个不需要
-        print(datas)
+        #print("data",data)
+        datas=data.decode(encoding='utf8').split(';') #最后一个不需要
+        #print("receivce %d sample."%len(datas[0:-1]))
         out=cnnmodel.predict(datas)
         print(out)
         #计算
         #发送结果
-        server_c.send(out)
+        server_c.send(bytes(out,encoding='utf8'))
 
