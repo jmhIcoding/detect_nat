be58fc3340a3106d3819d08f30a12cee26515003
DESKTOP-DJ29T4D\jmh081701 <1024836002@qq.com>
2018-04-07 11:18:50 +0800

    新增数据集增强 模块，主要是添加高斯噪声和NAT中ttl=1的样本

--- tool/database.py
+++ tool/database.py
@@ -3,6 +3,7 @@ __author__ = 'jmh081701'
 from pymongo import *
 import json
 import  time
+import  random
 class dat_filetool:
     def __init__(self,dat_filename):
         '''
@@ -66,8 +67,81 @@ class DataBase:
                     sample.setdefault('vec',each['vec'])
                     sample.setdefault('timestamp',each['timestamp'])
                     self.insert(sample)
+    def data_augmentation(self):
+        #数据集增强,主要通过计算正负样本的均值，方差，向正负样本加上高斯噪声
+        #高斯噪声
+
+        label="host"
+        data=self.get_dataset(cond={'label':label})
+        avg=list()
+        std=list()
+        veclen=len(data[0]['vec'])
+        sampleSize=len(data)
+        for j in range(0,veclen):
+            avg.append(0)
+            for i in range(0,sampleSize):
+                avg[j]+=data[i]['vec'][j]
+        for  j in range(0,veclen):
+            avg[j]/=sampleSize
+
+        for j in range(0,veclen):
+            std.append(0)
+            for i in range(0,sampleSize):
+                std[j]+=(data[i]['vec'][j]-avg[j])**2
+
+        for j in range(0,veclen):
+            std[j]/=(0.000001+sampleSize-1)
+            std[j]=std[j]**0.5+0.000001
+        for each in data:
+            for j in range(0,veclen):
+                #each['vec'][j]=(each['vec'][j]-avg[j])/std[j]
+                each['vec'][j]=0.95*each['vec'][j]+0.05*random.gauss(avg[j],std[j])
+            each.setdefault('tag','data_augmentation')
+            each.pop('_id')
+        self.insert(data)
+
+
+
+
+        label="nat"
+        data=self.get_dataset(cond={'label':label})
+        nat_data=data.copy()
+        avg=[]
+        std=[]
+        veclen=len(data[0]['vec'])
+        sampleSize=len(data)
+        for j in range(0,veclen):
+            avg.append(0)
+            for i in range(0,sampleSize):
+                avg[j]+=data[i]['vec'][j]
+        for  j in range(0,veclen):
+            avg[j]/=sampleSize
+
+        for j in range(0,veclen):
+            std.append(0)
+            for i in range(0,sampleSize):
+                std[j]+=(data[i]['vec'][j]-avg[j])**2
+
+        for j in range(0,veclen):
+            std[j]/=(0.000001+sampleSize-1)
+            std[j]=std[j]**0.5+0.000001
+        for each in data:
+            for j in range(0,veclen):
+                #each['vec'][j]=(each['vec'][j]-avg[j])/std[j]
+                each['vec'][j]=0.95*each['vec'][j]+0.05*random.gauss(avg[j],std[j])
+            each.setdefault('tag','data_augmentation_gauss')
+            each.pop('_id')
+        self.insert(data)
+        #将部分Nat的TTL个数改为1，因为也的确会有同一个局域网中所有主机的OS会是一致的情况.将比例控制在10%
+        for each in nat_data:
+            p=random.uniform(0,1)
+            if p<0.1 and each['vec'][-2]!=1:
+                each['vec'][-2]=1
+                each.setdefault('tag','data_augmentation_nat_synthes')
+                each.pop('_id')
+                self.insert(each)
 if __name__ == '__main__':
-    filereader= dat_filetool("./data/vectorize_data_03_29_mirror.dat")
+    filereader= dat_filetool("./data/vectorize_data_04_01_mirror.dat")
     dataset=filereader.reader()
     db=DataBase(ip="127.0.0.1")
     db.insert(dataset)
--- tool/dbmanager.py
+++ tool/dbmanager.py
@@ -2,7 +2,9 @@ __author__ = 'jmh081701'
 from database import  *
 db=DataBase(ip="127.0.0.1")
 #db.delete(cond={})
+#db.data_clean()
+#db.data_augmentation()
 dataset=db.get_dataset()
 print(len(dataset))
-print(dataset)
+#print(dataset)
 
