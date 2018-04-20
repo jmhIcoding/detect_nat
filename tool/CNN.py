9e539c376e169252b5d036592742f4179ca738ec
DESKTOP-DJ29T4D\jmh081701 <1024836002@qq.com>
2018-04-20 10:55:55 +0800

    添加ROC,混淆矩阵的保存

--- tool/CNN.py
+++ tool/CNN.py
@@ -8,8 +8,18 @@ import json
 '''
 The input vector should be a shape of [InputColumn,?]
 '''
-#dataset=dat_filetool("vectorize_data.dat").reader()
-data=Preprocess()
+#训练集
+dataset=None
+with open("all_data.dat") as fp:
+    dataset=json.load(fp)
+data=Preprocess(List=dataset)
+#测试集
+with open("normalize_parameter.data","r") as fp:
+    normalize_parameter=json.load(fp)
+raw_data=dat_filetool("./data/valid_data.dat").reader()
+valid=Preprocess(List=raw_data,SuperParameter=normalize_parameter,rate=0.0)
+
+
 InputColumn=6
 InputRow=int(data.veclen/InputColumn)
 InputX=tf.placeholder(dtype=tf.float32,shape=[None,None],name="InputX")
@@ -107,51 +117,128 @@ def save_model(sess,modelname="cnnmodel"):
     print("*"*30)
 merge_op=None
 merge_op2=None
-with tf.Session() as sess:
-    init =tf.global_variables_initializer()
-    sess.run(init)
+confuse_file_name="confuse_file"
 
-    step=1
-    sameMAX=40
-    sameStep=0
-    accSum=0
-    batch_epoch=int(data.num_example()/30)
-    load_model(sess)
-    while True:
-        if(step%batch_epoch==0):
-            #测试一下
-            test_vec,test_lab=data.next_test_batch(batchSize=30)
-            tf.summary.scalar('valid:rate',right_rate)
+for para in [1e-1,1e-2,1e-3,1e-4,1e-5,1e-6,1e-7]:
+    __test__vec=valid.train_vectors().copy()
+    __test__lab=valid.train_lables().copy()
+    confuse_file_name="confuse_file"
+    confuse_file_name="learning_rate"+str(para)+confuse_file_name
+    roc_file_name="roc_file"
+    roc_file_name="learning_rate"+str(para)+roc_file_name
+    with tf.Session() as sess:
+        init =tf.global_variables_initializer()
+        sess.run(init)
+        step=1
+        sameMAX=40
+        sameStep=0
+        accSum=0
+        batch_size=200
+        max_epoch=12000
+        learning_rate=para
+        batch_epoch=int(data.num_example()/batch_size)
+        #load_model(sess)
+        host_hostl=[]   #real->predict
+        host_natl=[]
+        nat_hostl=[]
+        nat_natl=[]
+        stepl=[]
 
-            if merge_op2==None:
-                merge_op2=tf.summary.merge_all()
-            acc,summary_=sess.run([right_rate,merge_op2],{InputX:test_vec,InputY:test_lab})
-            writer.add_summary(summary_,step)
-            print({"!!!!!!!!!!!!!!testing:"+str(step):acc})
-            accSum=accSum+acc
-            sameStep+=1
-            if(sameStep%sameMAX==0):
-                if(acc==accSum/sameMAX):
-                    print({step:acc})
-                    break
-                else:
-                    accSum=0
-                    sameStep=0
-                save_model(sess)
+        while True:
+            if(step%batch_epoch==0):
+                #测试一下
+                test_vec,test_lab=data.next_test_batch(batchSize=2000)
+                tf.summary.scalar('valid:rate',right_rate)
+
+                if merge_op2==None:
+                    merge_op2=tf.summary.merge_all()
+                acc,summary_,out=sess.run([right_rate,merge_op2,outputY],{InputX:test_vec,InputY:test_lab})
+                nat_nat=0
+                nat_host=0
+                host_nat=0
+                host_host=0
+                for i in range(len(test_lab)):
+                    if out[i][0]>out[i][1] :
+                        if test_lab[i][0]>test_lab[i][1]:
+                            host_host+=1
+                        else:
+                            nat_host+=1
+                    else:
+                        if test_lab[i][0]>test_lab[i][1]:
+                            host_nat+=1
+                        else:
+                            nat_nat+=1
+                host_hostl.append(host_host)
+                host_natl.append(host_nat)
+                nat_natl.append(nat_nat)
+                nat_hostl.append(nat_host)
+                writer.add_summary(summary_,step)
+                print({"!!!!!!!!!!!!!!testing:"+str(step):acc})
+                accSum=accSum+acc
+                sameStep+=1
+                if(sameStep%sameMAX==0):
+                    if(acc==accSum/sameMAX):
+                        print({step:acc})
+                        break
+                    else:
+                        accSum=0
+                        sameStep=0
+                    #save_model(sess)
+                step=step+1
+                stepl.append(step)
+                with open(confuse_file_name,"w") as fp :
+                    json.dump([host_natl,host_hostl,nat_hostl,nat_natl,stepl],fp)
+                continue
+            train_vec,train_lab=data.next_train_batch(batchSize=batch_size)
+            if merge_op==None:
+                merge_op=tf.summary.merge_all()
+            l,op,summary=sess.run([loss,train_op,merge_op],feed_dict={InputX:train_vec,InputY:train_lab})
+            #py,l,op=sess.run([predictY,loss,train_op])
+            print(step,l)
+            if(step%batch_size==0):
+                #每隔20批,跟踪一次
+                #writer.add_summary(summary,step)
+                pass
             step=step+1
-            continue
-        train_vec,train_lab=data.next_train_batch(batchSize=200)
-        if merge_op==None:
-            merge_op=tf.summary.merge_all()
-        l,op,summary=sess.run([loss,train_op,merge_op],feed_dict={InputX:train_vec,InputY:train_lab})
-        #py,l,op=sess.run([predictY,loss,train_op])
-        print(step,l)
-        if(step%20==0):
-            #每隔20批,跟踪一次
-            writer.add_summary(summary,step)
-            pass
-        step=step+1
-    save_model(sess)
+            if step>max_epoch:
+                print("Training Over....")
+                break
+        #计算ROC曲线
+        print("generate roc data.....")
+        host_hostl=[]   #real->predict
+        host_natl=[]
+        nat_hostl=[]
+        nat_natl=[]
+        out=sess.run("outputY:0",feed_dict={"InputX:0":__test__vec,"InputY:0":__test__lab})
+        #actual,predict
+        for threshold in range(0,1,step=0.05):
+            nat_nat=0
+            nat_host=0
+            host_nat=0
+            host_host=0
+            for i in range(len(__test__lab)):
+                print("predict:%s, real:%s ."%(str(out[i]),str(__test__lab[i])))
+                if out[i][1]<threshold :
+                    #预测为负样本host
+                    if __test__lab[i][0]>__test__lab[i][1]:
+                        host_host+=1
+                    else:
+                        nat_host+=1
+                else:
+                    #预测为正样本
+                    if __test__lab[i][0]>__test__lab[i][1]:
+                        host_nat+=1
+                    else:
+                        nat_nat+=1
+            nat_natl.append(nat_nat)
+            nat_hostl.append(nat_host)
+            host_hostl.append(host_host)
+            host_natl.append(host_nat)
+            print("TPR,FPR",1.0*nat_nat/(nat_host+nat_nat),1.0*host_nat/(host_host+host_nat))
+        with open(roc_file_name,"w") as fp:
+            json.dump([host_natl,host_hostl,nat_hostl,nat_natl],fp)
+
+    #save_model(sess)
     #print(data.test_vectors())
-    print(sess.run([right_rate],feed_dict={InputX:data.test_vectors(),InputY:data.test_labels()}))
+    #print(sess.run([right_rate],feed_dict={InputX:data.test_vectors(),InputY:data.test_labels()}))
 #注意点！在随机化参数的时候,不能标准差不能太大，否则很容易在计算log时出现nan
